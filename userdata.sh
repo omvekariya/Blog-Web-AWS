88c5a41220548878750325324c1d7d8f3d113ca4
Om Vekariya <omvekariya31@gmail.com>
2025-03-08 12:09:50 +0530

    Updated requirement.txt for mysqlclient

--- userdata.sh
+++ userdata.sh
@@ -3,13 +3,13 @@ apt-get update -y
 apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
-TOKEN="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"     # this token is no longer active
-git clone https://$TOKEN@github.com/rumeysakdogan/my-aws-capstone-project
-cd /home/ubuntu/my-aws-capstone-project
+TOKEN="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"
+git clone https://$TOKEN@github.com/omvekariya/Blog-Web-AWS
+cd /home/ubuntu/Blog-Web-AWS
 apt install python3-pip -y
 apt-get install python3.7-dev libmysqlclient-dev -y
 pip3 install -r requirements.txt
-cd /home/ubuntu/my-aws-capstone-project/src
+cd /home/ubuntu/Blog-Web-AWS/src
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
